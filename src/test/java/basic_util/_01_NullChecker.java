f5805b57cd95e2ba5f2017e8079dce7193ff4908
Dmytro Verbivskyi <dverbovskiy@gmail.com>
2017-04-09 23:13:09 +0300

    idea initial stuff

---
+++ src/test/java/basic_util/_01_NullChecker.java
@@ -0,0 +1,11 @@
+package basic_util;
+
+import org.junit.Test;
+
+public class _01_NullChecker {
+
+    @Test
+    public void name() throws Exception {
+        System.out.println("first");
+    }
+}
