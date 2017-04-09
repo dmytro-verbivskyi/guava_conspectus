6d6e7fbf463e5f9f128e08253c307c2fd6e9db4d
Dmytro Verbivskyi <dverbovskiy@gmail.com>
2017-04-10 00:41:50 +0300

    Optional.of() and Optional.fromNullable()

--- src/test/java/basic_util/_01_Optional.java
+++ src/test/java/basic_util/_01_Optional.java
@@ -8,11 +8,37 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class _01_Optional {
 
     @Test
-    public void happyOptional() throws Exception {
-        Optional<Integer> possible = Optional.of(5);
+    public void notNullIntoOf() throws Exception {
+        Integer input = 5;
+        Optional<Integer> possible = Optional.of(input);
 
         assertThat(possible.isPresent()).isTrue();
-        assertThat(possible.get()).isNotNull();
         assertThat(possible.get()).isEqualTo(5);
     }
+
+    @Test(expected = NullPointerException.class)
+    public void nullIntoOfMustThrowNullPointerException() throws Exception {
+        Integer input = null;
+        Optional.of(input);
+    }
+
+    @Test
+    public void notNullFromNullable() throws Exception {
+        Integer input = 5;
+        Optional<Integer> possible = Optional.fromNullable(input);
+
+        assertThat(possible.isPresent()).isTrue();
+        assertThat(possible.or(57)).isEqualTo(5);
+    }
+
+    @Test
+    public void nullRefWorkingAroundUseOrToProvideDefaultValue() throws Exception {
+        Integer input = null;
+        Optional<Integer> possible = Optional.fromNullable(input);
+
+        assertThat(possible.isPresent()).isFalse();
+        assertThat(possible.or(57)).isEqualTo(57);
+    }
+
+
 }
